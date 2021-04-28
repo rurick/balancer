75cab45ecd9bda09fd0c27ceafd81650976da292
Юрий Иовков <y.iovkov@digital-kitchen.ru>
2021-04-28 10:28:46 +0300

    Снято ограничение на запуск в отдельной горутине

--- balancer.go
+++ balancer.go
@@ -20,36 +20,38 @@ func New(val int) *Balancer {
 	return b
 }
 
-//Run - запустить процесс слежения. Всегда запускать отдельной горутиной
+//Run - запустить процесс слежения.
 /**ctx - контекст выполнения
  * minCPUUsage - минимальное значение использование CPU  в % когда нужно инкрементировать значение val
  * maxCPUUsage - максимальное значение использование CPU  в % когда нужно декрементировать значение val
  * maxVal - максимально допустимое значение val
  * */
 func (b *Balancer) Run(ctx context.Context, minCPUUsage int, maxCPUUsage int, maxVal int) {
-	cpu1 := systemstat.GetCPUSample()
-	cpu2 := cpu1
-	ticker := time.NewTicker(1 * time.Second)
+	go func() {
+		cpu1 := systemstat.GetCPUSample()
+		cpu2 := cpu1
+		ticker := time.NewTicker(1 * time.Second)
 
-	for {
-		select {
-		case <-ticker.C:
-			cpu1 = cpu2
-			cpu2 = systemstat.GetCPUSample()
-			st := systemstat.GetSimpleCPUAverage(cpu1, cpu2)
-			val := b.value.Load().(int)
-			if st.BusyPct > float64(maxCPUUsage) && val > 1 {
-				val--
+		for {
+			select {
+			case <-ticker.C:
+				cpu1 = cpu2
+				cpu2 = systemstat.GetCPUSample()
+				st := systemstat.GetSimpleCPUAverage(cpu1, cpu2)
+				val := b.value.Load().(int)
+				if st.BusyPct > float64(maxCPUUsage) && val > 1 {
+					val--
+				}
+				if st.BusyPct < float64(minCPUUsage) && val < maxVal {
+					val++
+				}
+				b.value.Store(val)
+			case <-ctx.Done():
+				ticker.Stop()
+				return
 			}
-			if st.BusyPct < float64(minCPUUsage) && val < maxVal {
-				val++
-			}
-			b.value.Store(val)
-		case <-ctx.Done():
-			ticker.Stop()
-			return
 		}
-	}
+	}()
 }
 
 //Value - получить значение
